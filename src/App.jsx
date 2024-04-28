cfbe30c31e2dae4c0586ceacb5c7edf40233f604
TLG442 <tharukalg@gmail.com>
2024-04-28 15:45:16 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -19,15 +19,15 @@ const App = () => {
     });
   });
   return (
-    <div className="">
+    <div>
       <div className="h-[700px] relative">
         <video
-          autoPlay
-          loop
-          muted
-          className="fixed right-0 top-0 h-[700px] w-full object-cover z-[-1]"
+               autoPlay
+               loop
+             muted
+             className="fixed right-0 top-0 h-[700px] w-full object-cover z-[-1]"
         >
-          <source src={BgVideo} type="video/mp4" />
+               <source src={BgVideo} type="video/mp4" />
         </video>
         <Navbar />
         <Hero />
--- src/components/Rapidscat/Rapidscat.jsx
+++ src/components/Rapidscat/Rapidscat.jsx
@@ -1,18 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import sateliteImg from "../../assets/satelite1.jpg";
 
 const Rapidscat = () => {
+  const [apodData, setApodData] = useState(null);
+    console.log(apodData)
+  useEffect(() => {
+    fetch("https://api.nasa.gov/planetary/apod?api_key=azrcPSop2fVRo8dUmVX7uwbWHAL2Qdu0xybgtiM7")
+      .then((response) => response.json())
+      .then((data) => setApodData(data))
+      .catch((error) => console.error("Error fetching APOD data: ", error));
+  }, []);
+
   return (
     <>
       <section className="bg-primary text-white pb-12">
         <div className="container ">
           <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 items-center">
             <div data-aos="zoom-in">
-              <img
-                src={sateliteImg}
-                alt=""
-                className="w-full sm:w-[80%] mx-auto max-h-[350px] object-cover"
-              />
+              {apodData && (
+                <img
+                  src={apodData.url}
+                  alt={apodData.title}
+                  className="w-full sm:w-[80%] mx-auto max-h-[350px] object-cover"
+                />
+              )}
             </div>
             <div className="space-y-3 xl:pr-36 p-4 border-r-2 border-b-2 border-r-sky-800 border-b-sky-800 ">
               <p
